f306542712a715df68926fffd4ed973c3b440d8a
Faskath513 <faskath9054@gmail.com>
2025-02-14 22:52:49 +0530

    clean

--- routes/feedbackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createFeedback, getFeedbackForUser } = require('../controllers/feedbackController');
-
-const router = express.Router();
-
-router.post('/', createFeedback); // Route to create feedback
-router.get('/:userId', getFeedbackForUser); // Route to get feedback for a specific user
-
-module.exports = router;
