e1a1895b194d9a1a7ceaeaca84cd55de4f807ecd
Muadh2002 <muadhmrm.22@uom.lk>
2025-02-18 22:03:06 +0530

    Updaetd department Routes

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -10,4 +10,14 @@ router.post('/create', authenticate, departmentController.createDepartment);
 // Get all departments
 router.get('/', authenticate, departmentController.getDepartments);
 
+router.get('/:id', authenticate, departmentController.getDepartmentById);
+
+// Update a department
+router.put('/:id', authenticate, departmentController.updateDepartment);
+
+// Delete a department
+router.delete('/:id', authenticate, departmentController.deleteDepartment);
+
+
+
 module.exports = router;
